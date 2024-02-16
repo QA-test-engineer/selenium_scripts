3de06e1464e9fb9b20b502e0960b8d1dba35c59c
User <User@DESKTOP-9R5J52G>
2024-02-16 18:45:06 +0530

    Pop up- File upload, Pop ups handling and multiple windows and Tab handling Test Script

--- src/com/testing/PopUpsWindow.java
+++ src/com/testing/PopUpsWindow.java
@@ -1,18 +1,24 @@
 package com.testing;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class PopUpsWindow {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\Documents\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();		
-		driver.get("https://www.hyrtutorials.com/p/window-handles-practice.html");
-
+		driver.get("https://admin:admin@the-internet.herokuapp.com/basic_auth");
+		
+		String text=driver.findElement(By.xpath("//div[@id='content' and @class='large-12 columns']")).getText();
+		
+		System.out.println(text);
+		Thread.sleep(2000);
+		driver.close();
 	}
 
 }
--- src/com/testing/windowHandling.java
+++ src/com/testing/windowHandling.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class windowHandling {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\Documents\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
@@ -16,13 +16,26 @@ public class windowHandling {
 		driver.manage().window().maximize();		
 		driver.get("https://www.hyrtutorials.com/p/window-handles-practice.html");
 		
-		String mainWindow=driver.getWindowHandle();
-		System.out.println(mainWindow);
+		String parentWindow=driver.getWindowHandle();
+		System.out.println(parentWindow);
 		
 		driver.findElement(By.id("newWindowBtn")).click();
 		Set<String> Handles=driver.getWindowHandles();
-		for(String handle : Handles) {
-			System.out.println(handle);
+		for(String childWindows : Handles) {
+			System.out.println(childWindows);
+			if(!childWindows.equals(parentWindow))
+			{
+				driver.switchTo().window(childWindows);
+				driver.findElement(By.id("firstName")).sendKeys("QA Engineer Uday");
+				Thread.sleep(5000);
+				driver.close();
+			}
+			
+			driver.switchTo().window(parentWindow);
+			driver.findElement(By.id("name")).sendKeys("Vasuki");
+			System.out.println(driver.findElement(By.id("name")).getTagName());
+			Thread.sleep(5000);	
+			driver.close();
 		}
 		
 	}
